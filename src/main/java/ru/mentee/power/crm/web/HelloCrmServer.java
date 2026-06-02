0ebf07110827f94f1ee87929c07eacb6a18ff15e
Ilya_Konovalov <ilia.mishchenko03@gmail.com>
2026-06-02 23:51:56 +0300

    BCORE-11: Добавлен HTTP сервер на JDK HttpServer с эндпоинтом /hello

---
+++ src/main/java/ru/mentee/power/crm/web/HelloCrmServer.java
@@ -0,0 +1,61 @@
+package ru.mentee.power.crm.web;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
+public class HelloCrmServer {
+
+    private final HttpServer server;
+    private final int port;
+
+    public HelloCrmServer (int port) throws IOException {
+        this.port = port;
+        this.server = HttpServer.create(new InetSocketAddress(port), 0);
+    }
+
+    public void start() {
+        server.createContext("/hello", new HelloHandler());
+        server.start();
+        System.out.println("Server started on http://localhost:" + port);
+    }
+
+    public void stop() {
+        server.stop(0);
+    }
+
+    static class HelloHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String method = exchange.getRequestMethod();
+            String path = exchange.getRequestURI().getPath();
+            System.out.println("Received " + method + " request for " + path);
+
+            String html =
+                    """
+                            <!DOCTYPE html>       
+                            <html>
+                              <head>
+                                <meta charset="UTF-8">   
+                                <title>Hello CRM!</title>       
+                              </head>
+                              <body>
+                                <h1>Hello CRM!</h1>           
+                              </body>
+                            </html>
+                    """;
+
+            byte[] responseByte = html.getBytes(StandardCharsets.UTF_8);
+            exchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
+            exchange.sendResponseHeaders(200, responseByte.length);
+            try(OutputStream os = exchange.getResponseBody()){
+                os.write(responseByte);
+            }
+        }
+    }
+}
